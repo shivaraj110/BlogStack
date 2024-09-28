da1ceb6d0e6774d6ed9558dbed418f565f484a6e
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-09-28 23:27:48 +0530

    added jwt in  signup endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode,jwt,sign,verify } from 'hono/jwt';
 
 
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string
+		DATABASE_URL: string,
+    JWT_SECRET:string
 	}
 }>();
 
@@ -18,15 +20,31 @@ app.post('/api/v1/user/signup',async (c) => {
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
 const body = await c.req.json()
-await prisma.user.create({
+const user = await prisma.user.findFirst({
+  where : {
+    email : body.email
+  }
+})
+if(user){
+c.json({
+  msg : "user already exists! try signing in directly..."
+})
+}
+const response = await prisma.user.create({
   data : {
     email : body.email,
     password : body.password
+  },
+  select : {
+    email : true,
+    id : true
   }
 })
-  return c.text('signup route')
+  const token = sign({id : response.id },c.env.JWT_SECRET)
+  return c.json({
+    jwt : token
+  })
 })
-
 
 
 app.post('/api/v1/user/signin', (c) => {
