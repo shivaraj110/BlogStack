7ec85343606c3fb5116ebd68140e7b4deaefbdf0
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-10-01 23:20:51 +0530

    made a data optional

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Blogs from "./pages/Blogs";
+import Blog from "./pages/Blog";
 import Landing from "./pages/Landing";
 import AllBlogs from "./pages/AllBlogs";
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
-          <Route path="blog/:id" element={<Blogs />} />
+          <Route path="blog/:id" element={<Blog/>} />
           <Route path="blogs" element={<AllBlogs />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/pages/AllBlogs.tsx
+++ frontend/src/pages/AllBlogs.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import Blogs from "../components/Blogs";
 
-function AllBlogs() {
-  return (
-    <div>AllBlogs</div>
-  )
+interface BlogData {
+  title: string;
+  content: string;
+  publishDate: string;
+  authorName: string;
 }
 
-export default AllBlogs
+function AllBlogs() {
+  const [blogs, setBlogs] = useState({});
+  useEffect(() => {}, []);
+  return (
+    <div>
+      <Blogs
+        authorName="shivaraj"
+        title="this is the title for this blog asssda sdadada dadafsomfs"
+        content=" this is content of this blog this is content of this blogthis is content of this blog this is content of this blog this is content of this blog this is content of this blog this is content of this blog this is content of this blog this is content of this blog this is content of this blog!"
+        publishDate="2 aug,2024"
+        context="docker"
+      />
+    </div>
+  );
+}
+
+export default AllBlogs;
