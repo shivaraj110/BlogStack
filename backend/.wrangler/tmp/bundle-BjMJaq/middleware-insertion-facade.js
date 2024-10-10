f1ecf1735e0c9ccac7eb31312c3f23b8d068f130
shivaraj-kc <shivarajchandaragi9@gmail.com>
2024-10-10 16:21:51 +0530

    added

---
+++ backend/.wrangler/tmp/bundle-BjMJaq/middleware-insertion-facade.js
@@ -0,0 +1,10 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\hp\\Documents\\medium\\backend\\src\\index.ts";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\hp\\Documents\\medium\\backend\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+
+				export * from "C:\\Users\\hp\\Documents\\medium\\backend\\src\\index.ts";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default
+				]
+				export default worker;
